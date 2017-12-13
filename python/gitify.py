83fa444f2a8f3d9ddc11c18e8b923bf72f29c004
Esteban De la Torre <a0227114@ti.com>
2017-12-12 17:23:10 -0800

    Update main to allow different gitify modes

--- python/gitify.py
+++ python/gitify.py
@@ -4,23 +4,25 @@ import shutil
 import argparse
 import subprocess
 
+MODE_INSTALLER = 'INSTALLER'
 
-def main(args):
-    if not os.path.exists(args.src_path):
-        sys.exit('Path does not exist: {}'.format(args.src_path))
+
+def gitify_installer(src_path, dest_path):
+    if not os.path.exists(src_path):
+        sys.exit('Path does not exist: {}'.format(src_path))
 
     # Get executable name: simplelink_XXXXXX_sdk_X_XX_XX_XX_eng.exe
-    full_name = os.path.basename(args.src_path)
+    full_name = os.path.basename(src_path)
 
     # Split the name and extension
     name, ext = os.path.splitext(full_name)
 
     # Destination path: C:\ti\simplelink_XXXXXX_sdk_X_XX_XX_XX_eng\
-    installer_dir = os.path.join(args.dest_path, name)
+    installer_dir = os.path.join(dest_path, name)
 
     # Copy executable to current working directory
     print('Copying...')
-    shutil.copy(args.src_path, '.')
+    shutil.copy(src_path, '.')
 
     # Run installer
     print('Installing...')
@@ -49,18 +51,31 @@ def main(args):
     os.remove(full_name)
 
 
+def main(args):
+    if args.mode == MODE_INSTALLER:
+        gitify_installer(args.src_path, args.dest_path)
+    else:
+        print('Invalid mode selected!'
+              '\nCurrent available modes:'
+              '\n\t- {}'
+              .format(MODE_INSTALLER))
+
+
 def parse_args():
     """
     Parses input parameters and displays help message
     :return: args: dict
     """
-    script_description = 'this script installs an installer and creates a repo'
+    script_description = ('this script creates a repo (gitifies) '
+                          'installers')
     parser = argparse.ArgumentParser(description=script_description)
+    parser.add_argument('mode',
+                        help='available modes: INSTALLER')
     parser.add_argument('src_path', metavar='PATH',
-                        help='installer zip file path')
+                        help='source path')
     parser.add_argument('--dest_path', metavar='PATH',
                         default=r'C:\ti',
-                        help='installer destination path')
+                        help='destination path')
     args = parser.parse_args()
     return args
 
